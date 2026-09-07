c53fc079a0e8081869e38bad3b616fdd2bef0fb1
onionmccabbage <onionmccabbage@live.com>
2026-09-07 10:29:12 +0100

    basics and patterns

--- basics.js
+++ basics.js
@@ -19,4 +19,14 @@ const fn = (x)=>{
 // NB Node is not run in a browser, so there is no DOM etc.
 console.log(old, trad(2))
 
+// this is a common pattern: makes good use of the run-loop
+const run = (n)=>{
+    if (n==0) {
+        throw new Error('We have reached zero')
+    }
+    run(n-1) // call itself
+}
+
+console.log(run(4))
+
 // when and why to use either function syntax....? 
