c92c2903c610638c82876deca1c8967265adc956
onionmccabbage <onionmccabbage@live.com>
2026-09-07 14:14:46 +0100

    sample solution for exercise 1

---
+++ exercise1.js
@@ -0,0 +1,55 @@
+// we can grab bits of Node to use in our own code (Node standard library)
+import { createServer } from 'http' // this is ESM - an ES Module
+import fs from 'node:fs'
+// const fs = require('fs') // commonJS ...or 'node:fs'
+// import * as http from 'http'
+
+// some handy config data
+const hostName = '127.0.0.1'
+const port = 3000
+
+// we can make our own server like this
+const server = createServer((req, res) => { // request and response objects
+    // explore sending html
+    // explore routing (conditional responses)
+    console.log(req.url) // url is the path
+    // append to a file (with a new line at the end) Notiee the empty call-back
+    fs.appendFile('serverlog.txt', `${req.url}\n`, () => {})
+    // act acording to the path
+    console.log(req.query)
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    if (req.url == '/') { // the home route
+        res.write('<h3>Welcome</h3>')
+        res.end()
+    }
+    else if (req.url == '/about') { // the about route
+        res.write('<h3>About this website...</h3>')
+        res.end()
+    }
+    else if (req.url == '/content') {
+        // read from file
+        fs.readFile('example.txt', 'utf8', (error, data) => {
+            if (error) {
+                console.log(error)
+                return
+            }
+            else if (data) {
+                console.log(data)
+                // send the file contents in the request response
+                res.write(data)
+                res.end()
+            }
+        })
+    }
+    else { // all other routes
+        res.write('<h3>404 Not Found</h3>')
+        res.end()
+    }
+
+})
+
+// we need to start our microserver
+server.listen(port, hostName, () => {
+    // we may use back-tick syntax to inject any pure JS statement
+    console.log(`Server running at http://${hostName}:${port}/`)
+})
