993408c96f6e28bf6d8fc210e015b44f2f51f2ff
onionmccabbage <onionmccabbage@live.com>
2026-09-07 15:26:58 +0100

    using export and import with ESM

--- 02_modules/app.js
+++ 02_modules/app.js
@@ -1,3 +1,5 @@
 // an app which import from elsewhere
-import {greet} from './util.mjs' // or '../util.mjs' a relative path
-console.log(greet('Ethel'))
+import {greet, arr} from './util.mjs' // or '../util.mjs' a relative path
+// import {arr} from './util.mjs'  // individual imports
+import DoNowt from './util.mjs' // no {} since this is the default
+console.log(greet('Ethel'), arr[6])
--- 02_modules/util.mjs
+++ 02_modules/util.mjs
@@ -3,3 +3,9 @@
 export const greet = (name)=>{
     return `Hello ${name}`
 }
+// NB an array can be sparse (empty members)
+export let arr = [5,4,3,2,7,,35,12]
+// export default means we import without {}
+export default class DoNowt{ 
+    // does nothing
+}
