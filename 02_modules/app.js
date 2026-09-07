a6ae3a4cc069e652ec261246c060f137cdee82e6
onionmccabbage <onionmccabbage@live.com>
2026-09-07 15:13:39 +0100

    simple ESM example

---
+++ 02_modules/app.js
@@ -0,0 +1,3 @@
+// an app which import from elsewhere
+import {greet} from './util.mjs' // or '../util.mjs' a relative path
+console.log(greet('Ethel'))
---
+++ 02_modules/util.mjs
@@ -0,0 +1,5 @@
+// utilities to be imported
+// NB we name this mjs to indicate it uses ESM
+export const greet = (name)=>{
+    return `Hello ${name}`
+}
