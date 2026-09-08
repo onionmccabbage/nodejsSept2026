5b2071511250be67b0788d8742c379863a3bd815
onionmccabbage <onionmccabbage@live.com>
2026-09-08 10:52:45 +0100

    example using thenable and also using async-await. Both use promises

--- 04_fetch/01_fetch.js
+++ 04_fetch/01_fetch.js
@@ -20,11 +20,23 @@ const fetchPersonPromise=()=>{
             console.error(err)
         })
     }
-
 // the same thing using async-await
+// we write the 'async' keyword before our function. 
+// This makes it a promise
+const fetchPlanetAsync = async ()=>{
+    const rand = getRandom()
+    const response = await fetch(`https://swapi.dev/api/planets/${rand}`)
+    // this next line will only run when the await has returned
+    return response.json()
+}
 
 // call our code
 fetchPersonPromise()
+    // entertain the user here....
     .then( (p)=>{
         console.log(p)
     } )
+
+// call our async code
+const planet = await fetchPlanetAsync()
+console.log(planet)
