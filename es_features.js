9a2852c3397d0f8790887e6331c6706a83dc4a96
onionmccabbage <onionmccabbage@live.com>
2026-09-09 11:28:34 +0100

    ES features

--- es_features.js
+++ es_features.js
@@ -8,8 +8,25 @@ const fnA = ()=>{}
 const fnB = ()=>'hello' // if there is only one statememnt, we can leave out the brackets
 const fnC = x=>x**2  // if there is exactly one argument, we can leave out the ()
 
-
-
-
 console.log(fnB())
 console.log(fnC(3))
+
+// everything above is in the global scope
+// we can create a new scope using {}
+{
+    // this is a separate scope
+    // unpacking with ...rest
+    let a = [3,4,5,6,2,7]
+    let [x,y,z, ...rest] = a // here we unpack the members of a  
+
+    console.log(x,y,z, rest)
+}
+{
+    // another scope
+    let a = {name:'Play School', presenter:'Floella', start:1976}
+    // unpack this object
+    const {name:n, presenter:p, ...rest} = a
+
+    console.log(`${n} presented by ${p} from ${rest.start}`)
+
+}
